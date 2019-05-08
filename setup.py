2db4269ddda73974c4c6034347465fb604d11f60
Atilla Ozgur <ati.ozgur@gmail.com>
2019-05-08 12:59:10 +0200

    version is beta py3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup(
    name = "Pyevolve",
-   version = '0.6',
+   version = '0.6.beta.p3',
    packages = ["pyevolve"],
    scripts = ['pyevolve_graph.py'],
    install_requires = ['future'],
