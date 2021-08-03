0cfcafb200cf2d9a2f4aa9a11a53e56a6181b52a
Atilla Ozgur <ati.ozgur@gmail.com>
2021-08-03 06:26:06 +0100

    change scripts so that it compiles in python 3

--- docs/build_docs_inc.bat
+++ docs/build_docs_inc.bat
@@ -1,2 +1,2 @@
 @echo off
-c:\python26\scripts\sphinx-build -b html .\source .\build
+sphinx-build -b html .\source .\build
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -185,8 +185,8 @@ htmlhelp_basename = 'Pyevolvedoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'Pyevolve.tex', ur'Pyevolve Documentation',
-   ur'Christian S. Perone', 'manual'),
+  ('index', 'Pyevolve.tex', 'Pyevolve Documentation',
+   'Christian S. Perone', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
