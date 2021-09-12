7dbf13c2d2538ad57992a3bf9234631feee9f67b
Atilla Ozgur <ati.ozgur@gmail.com>
2021-09-12 12:58:55 +0200

    imports fixed

--- examples/pyevolve_ex23_initial_population.py
+++ examples/pyevolve_ex23_initial_population.py
@@ -1,7 +1,7 @@
 from pyevolve.GPopulation import GPopulation
 from pyevolve.representations import G1DList
 from pyevolve import GSimpleGA
-from pyevolve import Selectors
+from pyevolve.selections import Selectors
 from pyevolve import DBAdapters
 
 
