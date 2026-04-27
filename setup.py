7a1b55965050ae3256a02cc0f348d39c4226a3b2
Atilla Özgür <ati.ozgur@gmail.com>
2026-04-27 14:44:04 +0300

    change package info in setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@
 from setuptools import setup
 import sys
 
+
 setup(
-   name = "Pyevolve",
+   name = "Pyevolve3",
    version = '0.7',
    packages = ["pyevolve"],
    scripts = ['pyevolve_graph.py'],
    package_data = {
       'pyevolve': ['*.txt']
-   },
+   }
    test_suite = 'tests',
-   author = 'Christian S. Perone',
-   author_email = "christian.perone@gmail.com",
-   description = "A complete, free and open-source evolutionary framework written in Python",
+   author = 'Atilla Özgür, Burak Dalkılıç, Christian S. Perone,   Gürkan Er',
+   author_email = "ati.ozgur@gmail.com",
+   description = "A complete, free and open-source genetic algorithms framework written in Python 3. Traveling salesperson problem related optimizations.",
    license = "PSF",
-   keywords = "genetic algorithm genetic programming algorithms framework library python ai evolutionary framework",
+   keywords = "genetic algorithms, travelling salesperson problem, framework library python ai evolutionary framework",
    url = "http://pyevolve.sourceforge.net/",
 )
