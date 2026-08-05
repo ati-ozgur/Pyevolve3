ed922575fc9b4a7e735d8d5955989608955900c2
Atilla Ozgur <ati.ozgur@gmail.com>
2026-08-05 15:09:27 +0300

    todo added

--- examples/helper_tsp.py
+++ examples/helper_tsp.py
@@ -1,3 +1,7 @@
+# todo: add log_file argument for logging info.
+# time, and other metrics should be added
+# todo: add matrix_file argument to load weights from matrix file
+
 import math
 import os
 import random
