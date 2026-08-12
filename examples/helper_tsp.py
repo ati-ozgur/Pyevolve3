92de372e969bae43d8f7f361556ee5bca75e673f
Atilla Özgür <ati.ozgur@gmail.com>
2026-08-12 21:50:05 +0300

    fix for python 3.9

--- examples/helper_tsp.py
+++ examples/helper_tsp.py
@@ -8,6 +8,8 @@ from pathlib import Path
 import random
 import time
 import inspect
+from typing import Optional
+
 
 import tsplib95
 
@@ -304,7 +306,7 @@ def get_coordinates_for_random_cities(
 
 def run_tsp(
     problem_name: str,
-    random_cities_info: dict | None = None,
+    random_cities_info: Optional[dict] = None,
     max_generation_count=100,
     crossover_rate=1.0,
     mutation_rate=0.02,
